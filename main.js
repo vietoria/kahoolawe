cea9e4f6a4e3963bcfd299ccc8bb035e610d3d1c
vietoria <vietoria.lam@gmail.com>
2018-10-11 07:12:27 +1100

    helpHWLP
    h

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+    background (201, 187, 169); 
+}
+   // const IMAGES = {
+	//title: 'https://i.imgur.com/kaKceHC.png', 
+//}; 
+
+  //var myFont; 
+  function preload() {
+  //myFont = loadFont('assets/sweetpea.ttf');
+
+ sect1 = loadImage('sect1.png');
+ sect2 = loadImage('sect2.png');
+ sect3 = loadImage('sect3.png');
+ sect4 = loadImage('sect4.png');
+ sect5 = loadImage('sect5.png');
+
+}
